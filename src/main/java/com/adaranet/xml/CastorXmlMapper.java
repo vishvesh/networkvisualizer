84a62481a595c8a2dcf92111255a6c2df22dcfce
vishvesh <Fragbait14>
2013-09-17 16:31:18 -0700

    Added utility marshal / unmarshall methods in CastorXmlMapper.java.... Need to create a few more/configure the existing methods to marshall/unmarshall the xml/object.

--- src/main/java/com/adaranet/xml/CastorXmlMapper.java
+++ src/main/java/com/adaranet/xml/CastorXmlMapper.java
@@ -1,5 +1,12 @@
 package com.adaranet.xml;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.Unmarshaller;
 
@@ -16,4 +23,27 @@ public class CastorXmlMapper {
 		this.unmarshaller = unmarshaller;
 	}
 
+	public void convertFromObjectToXML(Object object, String filepath) throws IOException {
+		FileOutputStream os = null;
+		try {
+			os = new FileOutputStream(filepath);
+			marshaller.marshal(object, new StreamResult(os));
+		} finally {
+			if (os != null) {
+				os.close();
+			}
+		}
+	}
+	 
+	public Object convertFromXMLToObject(String xmlfile) throws IOException {
+		FileInputStream is = null;
+		try {
+			is = new FileInputStream(xmlfile);
+			return unmarshaller.unmarshal(new StreamSource(is));
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
 }
